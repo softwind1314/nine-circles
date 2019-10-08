d9c762bd64eb96e80116bd637697c43269e9083f
softwind <softwind1314@gmail.com>
2019-10-08 13:10:41 +0800

    Implement first game demo
    new file:   main.py

---
+++ main.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+import curses
+
+scr = curses.initscr()
+
+# scr.addch(y,x, "a")
+# scr.refresh( y, x)
+
+#for i in range(10):
+#    scr.addch(10,i, str(i+1))
+
+#scr.refresh()
+
+def is_movable(g, i):
+    ''' G for down-side, if true, it is in down-side'''
+    if i == 0:
+        return True
+    if i == 1 and g[0] == False:
+        return True
+
+    if g[i-1] == False:
+        if False not in g[0:i-1]:
+            return True
+
+    return False
+
+
+import sys
+    
+if __name__ == "__main__":
+    curses.noecho()
+
+    scr = curses.initscr()
+
+    G =[False] * 9
+
+    for i in range(9):
+        if G[i]:
+            scr.addch(1,i,str(i+1))
+            scr.delch(0,i)
+        else:
+            scr.addch(0,i,str(i+1))
+            scr.delch(1,i)
+    scr.refresh()
+
+    while True:
+        ch_code = scr.getch()
+        ch = chr(ch_code)
+        if ch == 'q':
+            curses.endwin()
+            sys.exit(0)
+
+        idx = int(ch)
+        idx -= 1
+
+        ok = is_movable(G, idx)
+        if ok:
+            G[idx] = not G[idx]
+            for i in range(9):
+                if G[i]:
+                    scr.addch(1,i,str(i+1))
+                    scr.addch(0, i, " ")
+                else:
+                    scr.addch(0,i,str(i+1))
+                    scr.addch(1, i, " ")
+
+            scr.refresh()
+        else:
+            pass
+
